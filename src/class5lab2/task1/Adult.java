a2283efc251edaa831fb1904e5cc3f1ddf613095
Ashlee <Ashlee@10.30.12.71>
2017-09-20 19:56:07 -0500

    task 1 set up

--- src/class5lab2/task1/Adult.java
+++ src/class5lab2/task1/Adult.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package class5lab2;
+package class5lab2.task1;
 
 /**
  *
--- src/class5lab2/task1/Baby.java
+++ src/class5lab2/task1/Baby.java
@@ -1,4 +1,4 @@
-package class5lab2;
+package class5lab2.task1;
 
 
 public class Baby {
